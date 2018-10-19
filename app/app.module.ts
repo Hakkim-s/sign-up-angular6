6fab68a7f7855ff5f7b7721ae832a441674ed652
Hakkim S <40730845+Hakkim-s@users.noreply.github.com>
2018-10-19 23:45:31 +0530

    online register

---
+++ app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupComponent,
+    SigninComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
